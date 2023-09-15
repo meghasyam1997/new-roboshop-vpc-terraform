2a70a37f4409b135ebd60d27d1028c077a60632b
meghasyam1997 <meghasyam01@gmail.com>
2023-09-15 17:34:37 +0530

    assign the subnets

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ module "vpc" {
   for_each   = var.vpc
   source     = "git::https://github.com/meghasyam1997/new-vpc-terraform.git"
   cidr_block = each.value["cidr_block"]
+  subnets = each.value["subnets"]
   tags       = local.tags
   env        = var.env
 }
